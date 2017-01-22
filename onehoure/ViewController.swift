26c45707163dc73b6e088835dd7f3e2fe51b3e72
Jo Brunner <jo.brunner@mayflower.de>
2017-01-22 15:37:44 +0100

    Adds universal app layout

--- onehoure/ViewController.swift
+++ onehoure/ViewController.swift
@@ -1,15 +1,31 @@
+// Copyright (c) 2017 Jo Brunner
 //
-//  ViewController.swift
-//  onehoure
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-//  Created by Jo Brunner on 21.01.17.
-//  Copyright © 2017 Jo Brunner. All rights reserved.
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 import UIKit
 
+@IBDesignable
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var timerView: UIView!
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -27,6 +43,22 @@ class ViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         
         return true
+    }
+    
+    override func viewWillLayoutSubviews() {
+        
+        let size = view.bounds.size
+        let useWideDesign = size.width >= size.height
+        
+        if useWideDesign {
+            stackView.axis = .horizontal
+        }
+        else {
+            stackView.axis = .vertical
+        }
+
+        // hack for desing:
+        timerView.layer.cornerRadius = timerView.bounds.width/2
     }
 }
 
